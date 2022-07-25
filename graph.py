c4dae04eee71dc7beb753a0ac15a428628545c7b
sms.hZ <saeed.hazavehei@gmail.com>
2022-07-24 23:33:08 -0700

    set dunder method

--- graph.py
+++ graph.py
@@ -4,18 +4,30 @@ Created on Sun Jul 24 22:06:21 2022
 
 @author: Hazavei
 """
-#%% graph
+
+
+# %% graph
 class Graph:
     """all ploblem in graph"""
     def __init__(self):
         self.graph = {}
-        
-    def add_edg(self, start:str, end:str, weight:int=0):
-        self.graph.setdefault(start, []).append({end:weight})
-        
-        
-#%% code
+
+    def __str__(self):
+        return str(list(self.graph))
+
+    def __len__(self):
+        return self.graph.__len__()
+
+    def add_edg(self, start: str, end: str, weight: int = 0):
+        self.graph.setdefault(start, []).append({}.setdefault(end, weight))
+
+
+# %% code
 self = Graph()
 self.add_edg('A', 'c')
+self.add_edg('A', 'b')
+self.add_edg('b', 'c')
+self.add_edg('b', 'd')
+self.add_edg('d', 'c')
 
-self.graph
+print(self)
